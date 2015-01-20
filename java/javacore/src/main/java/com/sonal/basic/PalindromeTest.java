85a8deed488170788ea5ad179f4e9ff182e99628
ssinha <sonalkumar.sinha@warnerbros.com>
2015-01-20 13:12:51 -0800

    for palindrome

---
+++ java/javacore/src/main/java/com/sonal/basic/PalindromeTest.java
@@ -0,0 +1,30 @@
+package com.sonal.basic;
+
+public class PalindromeTest {
+
+	public static void main(String[] args) {
+		String inputString = "121";
+		checkPalindrome(inputString);
+		
+		inputString = "abc";
+		checkPalindrome(inputString);
+	}
+
+	private static void checkPalindrome(String inputString) {
+		int inputStringLength = inputString.length();
+		StringBuilder reverseInputString = new StringBuilder(inputStringLength);
+
+		for (int i = inputStringLength - 1; i >= 0; i--) {
+			reverseInputString.append(inputString.charAt(i));
+		}
+		
+		System.out.println(inputString);
+		System.out.println(reverseInputString);
+
+		if (inputString.equals(reverseInputString.toString())) {
+			System.out.println("Palindrome !!");
+		} else {
+			System.out.println("Not Palindrome !!");
+		}
+	}
+}
