431681f57d169f8e5c8f2fc82075c9fd5019a8ee
ssinha <sonalkumar.sinha@warnerbros.com>
2015-01-20 00:07:55 -0800

    for linked list in java

--- java/javacore/src/main/java/com/sonal/ds/linkedlist/MyLinkedList.java
+++ java/javacore/src/main/java/com/sonal/ds/linkedlist/MyLinkedList.java
@@ -1,56 +1,49 @@
 package com.sonal.ds.linkedlist;
 
 public class MyLinkedList {
-	
-	private Head head;  
-	private int size = 0;
-		
-	public MyLinkedList(){
+
+	private Head head;
+
+	public MyLinkedList() {
 		head = new Head();
-		size = 0;
 	}
-		
-	public void add(Object data){
+
+	public void add(Object data) {
 		Node newNode = new Node();
-		Node currentNode = null;
-		
-		currentNode = findCurrentNode();
-		
-		currentNode.setData(data);
-		currentNode.setNextNode(newNode);
-		size++;
-	}
-	
-	private Node findCurrentNode() {
-		Node currentNode = head.getNode();
-		if(currentNode == null){
-			currentNode = new Node();
-			head.setNode(currentNode);
-		}else{
-			for(int i = 0; i< size ; i++){
-				currentNode = currentNode.getNextNode();
-			}
-		}
-		
-		return currentNode;
-	}
-	
-	public void printAllElements(){
-		Node currentnode = head.getNode();
-		while(currentnode.getNextNode() != null){
-			System.out.println(currentnode.getData());
-			currentnode = currentnode.getNextNode();
+		newNode.setData(data);
+		newNode.setNextNode(null);
+
+		Node currentNode = findCurrentNode();
+
+		if (currentNode == null) {
+			head.setNode(newNode);
+		} else {
+			currentNode.setNextNode(newNode);
 		}
 	}
 
+	private Node findCurrentNode() {
+		Node currentNode = head.getNode();
+		if (currentNode != null) {
+			while (currentNode.getNextNode() != null) {
+				currentNode = currentNode.getNextNode();
+			}
+		}
+		return currentNode;
+	}
+
+	public void printAllElements() {
+		Node currentnode = head.getNode();
+
+		while(currentnode != null){
+			System.out.println(currentnode.getData());
+			currentnode = currentnode.getNextNode();
+		}
+
+	}
 
 	public Head getHead() {
 		return head;
 	}
 
-	public int getSize() {
-		return size;
-	}
-	
-	
 }
