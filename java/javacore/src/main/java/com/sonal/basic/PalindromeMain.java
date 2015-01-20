e26113e8bdaf58cc84ffc5856844a3b6bc16f917
ssinha <sonalkumar.sinha@warnerbros.com>
2015-01-20 13:54:47 -0800

    for string reverse using stringBuilder

--- java/javacore/src/main/java/com/sonal/basic/PalindromeMain.java
+++ java/javacore/src/main/java/com/sonal/basic/PalindromeMain.java
@@ -4,19 +4,39 @@ public class PalindromeMain {
 
 	public static void main(String[] args) {
 		String inputString = "121";
-		checkPalindrome(inputString);
+		checkPalindromeWay1(inputString);
 		
 		inputString = "abc";
-		checkPalindrome(inputString);
+		checkPalindromeWay1(inputString);
+		
+		inputString = "121";
+		checkPalindromeWay2(inputString);
+		
+		inputString = "abc";
+		checkPalindromeWay2(inputString);
 	}
 
-	private static void checkPalindrome(String inputString) {
+	private static void checkPalindromeWay1(String inputString) {
 		int inputStringLength = inputString.length();
 		StringBuilder reverseInputString = new StringBuilder(inputStringLength);
 
 		for (int i = (inputStringLength - 1); i >= 0; i--) {
 			reverseInputString.append(inputString.charAt(i));
 		}
+		
+		System.out.println(inputString);
+		System.out.println(reverseInputString);
+
+		if (inputString.equals(reverseInputString.toString())) {
+			System.out.println("Palindrome !!");
+		} else {
+			System.out.println("Not Palindrome !!");
+		}
+	}
+	
+	private static void checkPalindromeWay2(String inputString) {
+		
+		StringBuilder reverseInputString = new StringBuilder(inputString).reverse();
 		
 		System.out.println(inputString);
 		System.out.println(reverseInputString);
