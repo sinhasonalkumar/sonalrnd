72356efeb14a82b661e90c5eeb6b8c8914d32efe
ssinha <sonalkumar.sinha@warnerbros.com>
2015-01-29 23:17:01 -0800

    local variable init brush up

---
+++ java/javacore/src/main/java/com/sonal/localvariable/ClassMain.java
@@ -0,0 +1,17 @@
+package com.sonal.localvariable;
+
+public class ClassMain {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public void localVariableBehaviour(){
+		String s ;
+		int x;
+		// There is no default value of local variables neither for object and primitive types. They must be initialize to get some value.
+		//If we try to use it without initializing then compiler gives comiple time error.
+		//System.out.println(s);
+		//System.out.println(x);
+	}
+}
