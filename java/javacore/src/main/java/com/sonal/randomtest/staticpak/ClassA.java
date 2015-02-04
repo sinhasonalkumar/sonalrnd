0cf8e4163fa2d58d049379d927bc57bd29bd111a
ssinha <sonalkumar.sinha@warnerbros.com>
2015-02-03 21:23:33 -0800

    test commit

--- java/javacore/src/main/java/com/sonal/randomtest/staticpak/ClassA.java
+++ java/javacore/src/main/java/com/sonal/randomtest/staticpak/ClassA.java
@@ -3,6 +3,6 @@ package com.sonal.randomtest.staticpak;
 public class ClassA {
 	
 	public static void show(){
-		System.out.println("I am in static show");
+		System.out.println("I am in static show"); 
 	}
 }
