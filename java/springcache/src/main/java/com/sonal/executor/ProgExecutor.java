23448e2d80fb3710e8bc9355ca32375c7fc18f10
ssinha <sonalkumar.sinha@warnerbros.com>
2015-02-20 18:32:52 -0800

    for ssm

--- java/springcache/src/main/java/com/sonal/executor/ProgExecutor.java
+++ java/springcache/src/main/java/com/sonal/executor/ProgExecutor.java
@@ -5,16 +5,49 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.sonal.config.MemcachedCacheConfig;
 import com.sonal.services.CachedService;
-
+import com.sonal.vo.User;
 
 public class ProgExecutor {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MemcachedCacheConfig.class);
 		CachedService cachedService = applicationContext.getBean(CachedService.class);
-		
+
+		 computeFullName(cachedService);
+
+		cacheUserData(cachedService);
+
+	}
+
+	private static void cacheUserData(CachedService cachedService) {
+		User user1 = new User();
+		user1.setUserId("123");
+		user1.setUserName("test1");
+
+		User user2 = new User();
+		user2.setUserId("234");
+		user2.setUserName("test2");
+
+		String cacheKey = user1.getUserId();
+
+		user1 = cachedService.cacheSomeUserData(user1);
+		System.out.println(user1.getUserName());
+		user1 = cachedService.cacheSomeUserData(user1);
+		System.out.println(user1.getUserName());
+		user1 = cachedService.cacheSomeUserData(user1);
+		System.out.println(user1.getUserName());
+
+		cacheKey = user2.getUserId();
+		user2 = cachedService.cacheSomeUserData(user2);
+		System.out.println(user2.getUserName());
+		user2 = cachedService.cacheSomeUserData(user2);
+		System.out.println(user2.getUserName());
+		user2 = cachedService.cacheSomeUserData(user2);
+		System.out.println(user2.getUserName());
+	}
+
+	private static void computeFullName(CachedService cachedService) {
 		System.out.println(cachedService.computeFullName("1", "sonal", "sinha", "kumar"));
 		System.out.println(cachedService.computeFullName("1", "sonal", "sinha", "kumar"));
-		
 	}
 }
--- java/springcache/src/main/java/com/sonal/services/CachedService.java
+++ java/springcache/src/main/java/com/sonal/services/CachedService.java
@@ -4,20 +4,32 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
+import com.sonal.vo.User;
 
 @Service
 @EnableCaching
 public class CachedService {
 
-	//@Cacheable(value = "default",key = "#userId")
-	@Cacheable(value = "nameCache",key = "#userId")
-	public String computeFullName(String userId,String fname, String lname , String middleName){
+	// @Cacheable(value = "default",key = "#userId")
+	@Cacheable(value = "nameCache", key = "#userId")
+	public String computeFullName(String userId, String fname, String lname, String middleName) {
 		System.out.println("Computing Full Name : ");
 		String fullName = fname + " " + middleName + " " + lname;
 		System.out.println("Computed Full Name : " + fullName);
 		return fullName;
 	}
-	
-	
-	
+
+	//@Cacheable(value = "nameCache", key = "#user.userId", unless ="#user == null")
+	@Cacheable(value = "nameCache", key = "#user.userId")
+	public User cacheSomeUserData(User user) {
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return user;
+	}
+
 }
