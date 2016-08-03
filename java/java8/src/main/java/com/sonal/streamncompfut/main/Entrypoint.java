5dba1bacb3369d11fcd7805810319ccd3f93723f
sonal <sinha.sonal.kumar@gmail.com>
2016-08-03 00:06:55 -0700

    for forkjoin threadpool

--- java/java8/src/main/java/com/sonal/streamncompfut/main/Entrypoint.java
+++ java/java8/src/main/java/com/sonal/streamncompfut/main/Entrypoint.java
@@ -2,6 +2,8 @@ package com.sonal.streamncompfut.main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -10,8 +12,25 @@ import com.sonal.streamncompfut.vo.ServiceRequestVO;
 
 public class Entrypoint {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {	
+	
+	System.out.println("Main :: Thread ::" + Thread.currentThread().getId() + " :: " + Thread.currentThread().getName());
+	
+	ForkJoinPool pool = new ForkJoinPool(50);
 
+	pool.submit(Entrypoint::start);
+
+	pool.shutdown();	
+
+	try {
+	    pool.awaitTermination(1, TimeUnit.MINUTES);
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+
+    }
+
+    private static void start() {
 	SampleService sampleService = new SampleService();
 
 	List<ServiceRequestVO> serviceRequests = buildServiceRequests();
@@ -22,7 +41,6 @@ public class Entrypoint {
 		       .collect(Collectors.toList())
 		       .forEach(resultCF -> resultCF.whenComplete(Entrypoint :: receiveResult)
 			       			    .join());
-
     }
     
     public static void receiveResult(String result, Throwable t){
